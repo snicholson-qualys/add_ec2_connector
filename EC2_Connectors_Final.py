756240c2bccf2e0254b1113f2eafff06859ae120
Sean Nicholson <snicholson@qualys.com>
2018-11-20 16:24:46 -0500

    v1.0.0
    Initial commit

---
+++ EC2_Connectors_Final.py
@@ -0,0 +1,110 @@
+
+#
+# Author: Sean Nicholson
+# Purpose: Automate the adding of EC2 connectors via the Qualys API
+# version: 1.0.0
+# date: 11.20.2018
+#
+#
+#
+
+import sys, requests, datetime, os, time, yaml, json, csv, base64
+
+def config():
+    with open('config.yml', 'r') as config_settings:
+        config_info = yaml.load(config_settings)
+        username = str(config_info['defaults']['username']).rstrip()
+        password = str(config_info['defaults']['password']).rstrip()
+        URL = str(config_info['defaults']['apiURL']).rstrip()
+        if username == '' or password == '' or URL == '':
+            print "Config information in ./config.yml not configured correctly. Exiting..."
+            sys.exit(1)
+    return username, password, URL
+
+def Post_Call(username,password,URL,data_connector):
+
+    usrPass = str(username)+':'+str(password)
+    b64Val = base64.b64encode(usrPass)
+    headers = {
+        'Accept': '*/*',
+        'content-type': 'text/xml',
+        'X-Requested-With': 'curl',
+        'Authorization': "Basic %s" % b64Val
+
+    }
+
+    r = requests.post(URL, data=data_connector, headers=headers)
+    return r.raise_for_status()
+
+
+def Add_AWS_EC2_Connector():
+    username, password, URL = config()
+    URL = URL + "/qps/rest/2.0/create/am/awsassetdataconnector"
+
+    print '------------------------------AWS Connectors--------------------------------'
+    if not os.path.exists("debug"):
+        os.makedirs("debug")
+    debug_file_name = "debug/debug_file"+ time.strftime("%Y%m%d-%H%M%S") + ".txt"
+    debug_file = open(debug_file_name, "w")
+    debug_file.write('------------------------------AWS Connectors--------------------------------' + '\n')
+    with open('AWS_EC2_CONNECTOR_INFO.csv', 'rb') as f:
+        reader = csv.DictReader(f)
+        a = list(reader)
+        f.close()
+    counter=0
+    for i in a:
+        counter += 1
+
+        ARN = i['ARN']
+        EXT = i['EXTID']
+        NAME = i['NAME']
+        MODULE = i['MODULE']
+        REGION = i['REGION']
+        print str(counter) + ' : AWS Connector'
+        debug_file.write(str(counter) + ' : AWS Connector' + '\n')
+        print '---' + 'ARN : ' + str(ARN)
+        print '---' + 'EXT : ' + str(EXT)
+        #print '---' + 'DESC : ' + str(DESC)
+        print '---' + 'NAME : ' + str(NAME)
+        print '---' + 'REGION : ' + str(REGION)
+        print '---' + 'MODULE : ' + str(MODULE)
+        debug_file.write('---' + 'ARN : ' + str(ARN) + '\n')
+        debug_file.write('---' + 'EXT : ' + str(EXT) + '\n')
+        debug_file.write('---' + 'NAME : ' + str(NAME) + '\n')
+        debug_file.write('---' + 'REGION : ' + str(REGION) + '\n')
+        debug_file.write('---' + 'MODULE : ' + str(MODULE) + '\n')
+
+        module_list = i['MODULE'].split()
+        activate_module = ""
+        region_list = ""
+        activate_region = ""
+        for m in module_list:
+            activate_module += "<ActivationModule>{0}</ActivationModule>".format(str(m))
+        debug_file.write('---' + 'MODULE : ' + str(MODULE) + '\n')
+
+        if i['REGION'] != "ALL":
+            region_list = i['REGION'].split()
+            for r in region_list:
+                activate_region += "<regionCode>{0}</regionCode>".format(str(r))
+            debug_file.write('---' + 'REGION : ' + str(REGION) + '\n')
+            xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><ServiceRequest><data><AwsAssetDataConnector><name>{0}</name><arn>{1}</arn><externalId>{2}</externalId><endpoints><add><AwsEndpointSimple>{3}</AwsEndpointSimple></add></endpoints><disabled>false</disabled><activation><set>{4}</set></activation></AwsAssetDataConnector></data></ServiceRequest>".format(i['NAME'],i['ARN'],i['EXTID'],i['REGION'],activate_module)
+
+        else:
+            xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><ServiceRequest><data><AwsAssetDataConnector><name>{0}</name><arn>{1}</arn><externalId>{2}</externalId><disabled>false</disabled><allRegions>true</allRegions><activation><set>{3}</set></activation></AwsAssetDataConnector></data></ServiceRequest>".format(i['NAME'],i['ARN'],i['EXTID'],activate_module)
+
+        try:
+            Post_Call(username, password, URL, xml)
+            print str(counter) + ' : Connector Added Successfully'
+            print '-------------------------------------------------------------'
+            debug_file.write(str(counter) + ' : Connector Added Successfully' + '\n')
+
+        except requests.exceptions.HTTPError as e:  # This is the correct syntax
+            print str(counter) + ' : Failed to Add AWS Connector'
+            print e
+            print '-------------------------------------------------------------'
+            debug_file.write(str(counter) + ' : Failed to Add AWS Connector' + '\n')
+            debug_file.write(str(e) + '\n')
+
+    debug_file.close()
+
+Add_AWS_EC2_Connector()
--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # add_ec2_connector
+Info : Python File adds the AWS EC2 connector into Qualys Asset View Connectors w.r.t details provided in "./AWS_EC2_CONNECTOR_INFO.csv" & "./config.txt".
+Console output as well as debug_file.txt will have both success & failure logs.
+
+#AWS_EC2_CONNECTOR_INFO.csv:
+Info : csv files contains below attributes required for AWS connector
+Script looks for AWS_CONNECTOR_INFO.csv in the directory the script runs from
+> ARN,NAME,EXTID,REGION,MODULE
+
+*EXTID must be an INT from 9-90 in length* Example: 98765456787654567821
+
+*REGION must be single spaced list of regions or "ALL"*
+Currently only ALL and single specified region work. Additonal support for multiple regions is being worked.
+*Examples*
+> ALL
+> us-east-1
+
+
+*MODULES must be an list of modules to activate for connector*
+*Examples:*
+> VM
+> "VM PC"
+> "VM PC SCA"
+
+#Script configuration
+*config.yml*
+Provide script configuration information for API U/P, vulnerability severity ratings, and Qualys API URL
+  username: "QualysUsername"
+  password: "QualysPassword"
+
+  apiURL: "Qualys API URL base (https:// - > .com/, no trailing '/') for your pod"
+  Examples:
+  >https://qualysapi.qualys.com
+  >https://qualysapi.qg2.apps.qualys.com
+
+
+#Script Requirements
+This script is written in Python 2.7.x (X > 10)
+This script requires the following PIP modules to run
+Modules: sys, requests, datetime, os, time, yaml, json, csv, base64
+
+Example Python module install
+MAC/Linux "pip install requests"
+Windows "python -m pip install requests"
+
+Example Python yaml module install
+MAC/Linux "pip install pyyaml"
+Windows "python -m pip install pyyaml"
+
+
+#Debug
+Debug file for script run, located in ./debug folder with time/date stamp per line. To disable debug, comment out all lines containing "debug"
